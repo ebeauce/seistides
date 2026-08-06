02285b8ecc111144053b8a13300e2f4ee0589edc
Eric Beauce <ebeauce@hypo-8.ldeo.columbia.edu>
2026-08-06 16:20:09 -0400

    plotting method

--- seistides/modulation.py
+++ seistides/modulation.py
@@ -298,6 +298,54 @@ class Modulationmeter(ABC):
                 100. * (1. - np.sum(residuals_proposed_model**2) / np.sum(obs_c**2))
                 )
 
+    def plot_modulation(
+            self, forcing, window_name, ymin=0.75, ymax=1.25, model_name=None, **kwargs
+            ):
+        """
+        """
+        import matplotlib.pyplot as plt
+        from matplotlib.colors import Normalize
+        from matplotlib.cm import ScalarMappable
+
+        norm = Normalize(vmin=ymin, vmax=ymax)
+        scalar_map = ScalarMappable(
+                norm=norm, cmap=kwargs.get("cmap", plt.get_cmap("coolwarm"))
+                )
+
+        fig, ax = plt.subplots()
+        _mod = self.modulation[forcing][window_name]
+        _bins = _mod["bins"]
+        _midbins = _mod["midbins"]
+        ax.bar(
+                _bins[:-1],
+                _mod["relative_rate"],
+                yerr=_mod["relative_rate_err"],
+                width=_bins[1]-_bins[0],
+                edgecolor=kwargs.get("edgecolor", "none"),
+                align=kwargs.get("align", "edge"),
+                color=scalar_map.to_rgba(_mod["relative_rate"]),
+                )
+        ax.axhline(1.0, lw=0.50, color="k")
+
+        if model_name is not None:
+            if window_name in self.model[forcing]:
+                _model = self.model[forcing][window_name][model_name]
+                fit = _model["func"](np.deg2rad(_midbins))
+                ax.plot(_midbins, fit, color="k", label="Model")
+
+        xtick_loc = [-180.0, -90.0, 0, 90.0, 180.0]
+        xtick_lab = [r"$-\pi$", r"$-\frac{\pi}{2}$", r"$0$", r"$\frac{\pi}{2}$", r"$\pi$"]
+        ax.set_xticks(xtick_loc)
+        ax.set_xticklabels(xtick_lab)
+
+        ax.set_ylabel(r"Relative rate of seismicity")
+        ax.set_xlabel(r"Tidal phase, $\phi$ (rad)")
+
+        ax.grid()
+        ax.set_ylim(ymin, ymax)
+
+        return fig
+
 
     def set_catalog(self, catalog):
         if catalog is None:
